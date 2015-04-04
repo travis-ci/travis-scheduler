6204ca6e308ab91ddc1e98a47fbb71ad945b3c45
Josh Kalderimis <josh.kalderimis@gmail.com>
2015-04-04 14:52:45 +1300

    damn commas

--- lib/travis/scheduler/config.rb
+++ lib/travis/scheduler/config.rb
@@ -12,7 +12,7 @@ module Travis
               redis:          { url: 'redis://localhost:6379' },
               sentry:         { },
               sidekiq:        { namespace: 'sidekiq', pool_size: 3 },
-              ssl:            { },
+              ssl:            { }
 
       default _access: [:key]
 
