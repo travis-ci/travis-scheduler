52260deff2dde9038643c5dcc2c49a476d1a95ce
Josh Kalderimis <josh.kalderimis@gmail.com>
2015-04-04 14:39:12 +1300

    make sure the test db is used during testing

--- spec/support/active_record.rb
+++ spec/support/active_record.rb
@@ -8,6 +8,7 @@ FileUtils.mkdir_p('log')
 # TODO: why not make this use Travis::Database.connect ?
 config = Travis.config.database.dup
 config.merge!('adapter' => 'jdbcpostgresql', 'username' => ENV['USER']) if RUBY_PLATFORM == 'java'
+config['database'] = "travis_test"
 
 ActiveRecord::Base.default_timezone = :utc
 ActiveRecord::Base.logger = Logger.new('log/test.db.log')
