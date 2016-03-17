ef051ac0679799b252aeea83369cdd0cc34384bc
Tyranja <tyranja@cassiopeia.uberspace.de>
2016-03-17 13:26:00 +0100

    Add a debugging method

--- lib/travis/scheduler/models/job/config.rb
+++ lib/travis/scheduler/models/job/config.rb
@@ -6,6 +6,7 @@ class Job
   module Config
     def self.decrypt(config, decryptor, options)
       config = config.deep_dup
+      p config
       config = Config::Normalize.new(config, options).apply
       config = Config::Decrypt.new(config, decryptor, options).apply
       config
