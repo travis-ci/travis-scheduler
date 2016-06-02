13e2639f4d666b80a39dd4d3dd42ce6ec743c1a0
Sven Fuchs <me@svenfuchs.com>
2016-06-02 12:06:00 +0200

    add specs for when we add an ssh_key to the worker payload

--- lib/travis/scheduler/payloads/worker.rb
+++ lib/travis/scheduler/payloads/worker.rb
@@ -107,7 +107,6 @@ module Travis
           elsif ssh_key = job.ssh_key
             { 'source' => 'travis_yaml', 'value' => ssh_key, 'encoded' => true }
           else
-            ssh_key = repository.key.private_key
             { 'source' => 'default_repository_key', 'value' => repository.key.private_key, 'encoded' => false }
           end
         end
