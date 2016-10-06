069b2c37c3be98e3aa0c7b07b05505c3507d7877
Sven Fuchs <me@svenfuchs.com>
2016-10-06 20:06:25 +0200

    symbolize keys in worker

--- lib/travis/scheduler/worker.rb
+++ lib/travis/scheduler/worker.rb
@@ -13,12 +13,24 @@ module Travis
       private
 
         def normalize(args)
+          args = symbolize_keys(args)
           args.last[:jid] ||= jid if args.last.is_a?(Hash)
           args
         end
 
         def context
           Scheduler.context
+        end
+
+        def symbolize_keys(obj)
+          case obj
+          when Array
+            obj.map { |obj| symbolize_keys(obj) }
+          when ::Hash
+            obj.map { |key, value| [key.to_sym, symbolize_keys(value)] }.to_h
+          else
+            obj
+          end
         end
     end
   end
