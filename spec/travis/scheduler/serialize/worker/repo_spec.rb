0182fb12833ba4b46abc3cd376d0cd09d431a962
Hiro Asari <asari.ruby@gmail.com>
2016-11-23 08:07:43 -0500

    Let Travis.config.prefer_https override git URL scheme

--- spec/travis/scheduler/serialize/worker/repo_spec.rb
+++ spec/travis/scheduler/serialize/worker/repo_spec.rb
@@ -36,5 +36,39 @@ describe Travis::Scheduler::Serialize::Worker::Repo do
         it { expect(subject.source_url).to eq 'git@localhost:travis-ci/travis-ci.git' }
       end
     end
+
+    context "when config prefers HTTPS source_url" do
+      before(:all)  { @before = Travis.config.prefer_https }
+      before(:each) { Travis.config.prefer_https = true }
+      after(:all)   { Travis.config.prefer_https = @before }
+
+      describe 'default source endpoint' do
+        before { config[:github][:source_host] = 'github.com' }
+
+        describe 'on a public repo' do
+          before { repo.private = false }
+          it { expect(subject.source_url).to eq 'https://github.com/travis-ci/travis-ci.git' }
+        end
+
+        describe 'on a private repo' do
+          before { repo.private = true }
+          it { expect(subject.source_url).to eq 'https://github.com/travis-ci/travis-ci.git' }
+        end
+      end
+
+      describe 'custom source endpoint' do
+        before { config[:github][:source_host] = 'localhost' }
+
+        describe 'on a public repo' do
+          before { repo.private = false }
+          it { expect(subject.source_url).to eq 'https://localhost/travis-ci/travis-ci.git' }
+        end
+
+        describe 'on a private repo' do
+          before { repo.private = true }
+          it { expect(subject.source_url).to eq 'https://localhost/travis-ci/travis-ci.git' }
+        end
+      end
+    end
   end
 end
