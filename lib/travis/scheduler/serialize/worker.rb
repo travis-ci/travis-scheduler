517b33aa2acc9652852b74e4a591c4099e238693
Hiro Asari <asari.ruby@gmail.com>
2016-12-07 19:39:51 -0500

    Put repo admin's github_oauth_token on payload

--- lib/travis/scheduler/serialize/worker.rb
+++ lib/travis/scheduler/serialize/worker.rb
@@ -22,7 +22,8 @@ module Travis
             repository: repository_data,
             ssh_key: ssh_key,
             timeouts: repo.timeouts,
-            cache_settings: cache_settings
+            cache_settings: cache_settings,
+            oauth_token: github_oauth_token
           }
         end
 
@@ -113,6 +114,14 @@ module Travis
 
           def format_date(date)
             date && date.strftime('%Y-%m-%dT%H:%M:%SZ')
+          end
+
+          def github_oauth_token
+            admin = job.repository.users.find do |u|
+              ! u.github_oauth_token.nil?
+            end
+
+            admin.github_oauth_token
           end
       end
     end
