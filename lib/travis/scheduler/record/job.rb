e1e860a85d75fcd7b606c638176997e56a8da7b6
Sven Fuchs <me@svenfuchs.com>
2017-04-12 21:56:22 +0200

    use queueable by default, make state opt in

--- lib/travis/scheduler/record/job.rb
+++ lib/travis/scheduler/record/job.rb
@@ -5,8 +5,8 @@ class Job < ActiveRecord::Base
     }
 
     def queueable
-      jobs = where(state: :created).order(:id)
-      jobs = jobs.joins(SQL[:queueable]).order(:id) if ENV['USE_QUEUEABLE_JOBS']
+      jobs = joins(SQL[:queueable]).order(:id)
+      jobs = jobs.where(state: :created).order(:id) if ENV['USE_JOB_STATE']
       jobs
     end
 
