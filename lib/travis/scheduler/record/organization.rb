b2b6904c2a9d10e8a70e1679e0e00233e887a0d8
Renée Hendricksen <renee@travis-ci.org>
2017-05-16 20:51:53 -0700

    fixing subscription retrieval inconsistencies and making a change for existing subscribers who sign up through marketplace

--- lib/travis/scheduler/record/organization.rb
+++ lib/travis/scheduler/record/organization.rb
@@ -1,5 +1,9 @@
 class Organization < ActiveRecord::Base
-  has_one :subscription, as: :owner
+
+  def subscription
+    subs = Subscription.where(owner_id: id, owner_type: "Organization")
+    @subscription ||= subs.where(status: 'subscribed').last || subs.last
+  end
 
   def subscribed?
     subscription.present? and subscription.active?
--- lib/travis/scheduler/record/user.rb
+++ lib/travis/scheduler/record/user.rb
@@ -2,8 +2,12 @@ require 'active_record'
 require 'travis/support/encrypted_column'
 
 class User < ActiveRecord::Base
-  has_one :subscription, as: :owner
   serialize :github_oauth_token, Travis::EncryptedColumn.new
+
+  def subscription
+    subs = Subscription.where(owner_id: id, owner_type: "User")
+    @subscription ||= subs.where(status: 'subscribed').last || subs.last
+  end
 
   def subscribed?
     subscription.present? and subscription.active?
