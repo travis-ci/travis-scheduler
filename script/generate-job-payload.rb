176cea8019914dce027a12e06c698d34c1e2c524
AJ Bowen <aj@soulshake.net>
2017-08-11 16:46:47 +0200

    Add script/generate-job-payload.rb

---
+++ script/generate-job-payload.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+# This script takes a job id and spits out the job payload for that id
+# Usage:
+#`heroku run -a travis-scheduler-<stage> script/generate-job-payload <job ID>`
+
+$: << 'lib'
+require 'bundler/setup'
+require 'travis/scheduler'
+
+# Silence Raven so output can be piped directly elsewhere.
+Travis::Scheduler.logger = Logger.new(IO::NULL)
+
+require 'raven/logger'
+module Raven
+  class Logger
+    def add(*)
+    end
+  end
+end
+
+Travis::Scheduler.setup
+
+id = Integer(ARGV[0])
+job = Job.find(id)
+config = Travis::Scheduler::Config.load
+payload = Travis::Scheduler::Serialize::Worker.new(job, config).data
+puts JSON.generate(payload)
