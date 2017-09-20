fb43752a0c2bddf46987cb3f9c40f16c6ab78e48
Sven Fuchs <me@svenfuchs.com>
2017-09-20 17:37:38 +0200

    allow setting a global default for the by_queue limit

--- spec/travis/scheduler/limit_spec.rb
+++ spec/travis/scheduler/limit_spec.rb
@@ -116,7 +116,32 @@ describe Travis::Scheduler::Limit::Jobs do
     it { expect(report).to include('user svenfuchs: total: 7, running: 3, queueable: 2') }
   end
 
-  describe 'with a by_queue limit of 2' do
+  describe 'with no by_queue config being given' do
+    before { create_jobs(9, queue: 'builds.osx') }
+    before { create_jobs(1, queue: 'builds.docker') }
+    before { config.limit.default = 99 }
+    before { subject }
+
+    it { expect(subject.size).to eq 10 }
+    it { expect(report).to include('user svenfuchs: total: 10, running: 0, queueable: 10') }
+  end
+
+  describe 'with a default by_queue limit of 2' do
+    before { create_jobs(9, queue: 'builds.osx') }
+    before { create_jobs(1, queue: 'builds.docker') }
+    before { config.limit.default = 99 }
+    before { ENV['BY_QUEUE_DEFAULT'] = '2' }
+    before { ENV['BY_QUEUE_NAME'] = 'builds.osx' }
+    after  { ENV.delete('BY_QUEUE_LIMIT') }
+    after  { ENV.delete('BY_QUEUE_NAME') }
+    before { subject }
+
+    it { expect(subject.size).to eq 3 }
+    it { expect(report).to include('max jobs for user svenfuchs by queue builds.osx: 2') }
+    it { expect(report).to include('user svenfuchs: total: 10, running: 0, queueable: 3') }
+  end
+
+  describe 'with a by_queue limit of 2 for the owner' do
     before { create_jobs(9, queue: 'builds.osx') }
     before { create_jobs(1, queue: 'builds.docker') }
     before { config.limit.default = 99 }
@@ -131,7 +156,7 @@ describe Travis::Scheduler::Limit::Jobs do
     it { expect(report).to include('user svenfuchs: total: 10, running: 0, queueable: 3') }
   end
 
-  describe 'with a by_queue limit of 2 and a repo limit of 3 on another repo' do
+  describe 'with a by_queue limit of 2 for the owner and a repo limit of 3 on another repo' do
     let(:other) { FactoryGirl.create(:repo, github_id: 2) }
     before { create_jobs(9, repository: repo, queue: 'builds.osx') }
     before { create_jobs(5, repository: other, queue: 'builds.docker') }
@@ -148,7 +173,7 @@ describe Travis::Scheduler::Limit::Jobs do
     it { expect(report).to include('user svenfuchs: total: 14, running: 0, queueable: 5') }
   end
 
-  describe 'with a by_queue limit and jobs created for a different queue' do
+  describe 'with a by_queue limit for the owner and jobs created for a different queue' do
     before { create_jobs(9, queue: 'builds.docker') }
     before { create_jobs(1, queue: 'builds.osx') }
     before { config.limit.default = 5 }
