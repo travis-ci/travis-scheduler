af36582b6503aab9f7f3922a95c45e19a096b358
Kerri Miller <kerrizor@kerrizor.com>
2018-04-27 12:34:22 -0700

    Add #default_worker_timeout with logic for paid/unpaid switching

--- lib/travis/scheduler/record/user.rb
+++ lib/travis/scheduler/record/user.rb
@@ -17,6 +17,20 @@ class User < ActiveRecord::Base
     redis.get("trial:#{login}").to_i > 0
   end
 
+  def default_worker_timeout
+    # When the user is a paid user ("subscribed") or has an active trial, they
+    #   are granted a different default timeout on their jobs.
+    #
+    # Note that currently (27/4/18) we are NOT providing different timeouts, but
+    #   we plan to sometime in the following weeks/months.
+    #
+    if subscribed? || active_trial?
+      120 * 60 # timeouts are in seconds
+    else
+      60 * 60
+    end
+  end
+
   private
 
   def redis
