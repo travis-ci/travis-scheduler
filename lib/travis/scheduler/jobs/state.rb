56999808ddfd5e4b1d4d4baf5860224fca6e7a0c
Sven Fuchs <me@svenfuchs.com>
2018-06-29 13:41:56 +0200

    add back queueable jobs metric

--- lib/travis/scheduler/jobs/state.rb
+++ lib/travis/scheduler/jobs/state.rb
@@ -1,22 +1,24 @@
+require 'travis/scheduler/helper/context'
+require 'travis/scheduler/helper/metrics'
+
 module Travis
   module Scheduler
     module Jobs
       class State < Struct.new(:context, :owners)
+        include Helper::Context, Helper::Metrics
+
         ATTRS = {
           running:  %i(repository_id private queue),
           by_build: %i(id state stage_number)
         }
 
-        # this reads all of the owners' currently running jobs into memory.
-        # is this preferable? should we run several db queries instead?
         def running
-          @running ||= Job.by_owners(owners.all).running.select(*ATTRS[:running]).to_a
+          @running ||= read_running
         end
 
         def queueable
-          @queueable ||= Job.by_owners(owners.all).queueable.to_a
+          @queueable ||= read_queueable
         end
-        # time :queueable, key: 'scheduler.queueable_jobs'
 
         def by_build(id)
           cache[id] ||= Job.where(source_id: id).select(*ATTRS[:by_build]).to_a
@@ -39,6 +41,16 @@ module Travis
         end
 
         private
+
+          def read_running
+            Job.by_owners(owners.all).running.select(*ATTRS[:running]).to_a
+          end
+          time :read_queueable, key: 'scheduler.running_jobs'
+
+          def read_queueable
+            Job.by_owners(owners.all).queueable.to_a
+          end
+          time :read_queueable, key: 'scheduler.queueable_jobs'
 
           def cache
             @cache ||= {}
