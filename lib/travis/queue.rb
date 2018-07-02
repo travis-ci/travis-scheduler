6eb210d1d3d362f15bcb524529cffe1696fc804b
Sven Fuchs <me@svenfuchs.com>
2018-07-02 16:34:59 +0200

    paid/sponsored pools

--- lib/travis/queue.rb
+++ lib/travis/queue.rb
@@ -1,10 +1,18 @@
 module Travis
   class Queue < Struct.new(:job, :config, :logger)
     def select
-      queue.try(:name) || config[:queue][:default]
+      with_pool(name)
     end
 
     private
+
+      def with_pool(name)
+        Pool.new(job, name).to_s
+      end
+
+      def name
+        queue.try(:name) || config[:queue][:default]
+      end
 
       def queue
         queues.detect { |queue| matcher.matches?(queue.attrs) }
@@ -22,6 +30,6 @@ end
 
 require 'travis/queue/sudo'
 require 'travis/queue/matcher'
+require 'travis/queue/pool'
 require 'travis/queue/queues'
 require 'travis/queue/sudo_detector'
-
