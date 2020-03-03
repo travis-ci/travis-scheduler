ebfbc76a5e910e82c98c6c0c07c5a218bcfc4890
Artur Kremens <artur.kremens@gmail.com>
2020-03-03 10:27:33 +0100

    User vcs_source_host directly from the db (#220)
    * Use repo vcs_slug when persisted
    
    * User vcs_source_host directly from the db
    
    * User vcs_source_host directly from the db
    
    * Add delegator

--- lib/travis/scheduler/config.rb
+++ lib/travis/scheduler/config.rb
@@ -25,8 +25,7 @@ module Travis
              ping:       { interval: 5 * 60 },
              site:       ENV['TRAVIS_SITE'] || 'org',
              ssl:        { },
-             job_board:  { url: ENV['JOB_BOARD_URL'] || 'https://job-board.travis-ci.org', auth: ENV['JOB_BOARD_AUTH'] || 'user:pass' },
-             vcs: { cache_ex: 600 }
+             job_board:  { url: ENV['JOB_BOARD_URL'] || 'https://job-board.travis-ci.org', auth: ENV['JOB_BOARD_AUTH'] || 'user:pass' }
 
       def metrics
         # TODO fix keychain?
--- lib/travis/scheduler/serialize/worker.rb
+++ lib/travis/scheduler/serialize/worker.rb
@@ -9,7 +9,6 @@ module Travis
         require 'travis/scheduler/serialize/worker/request'
         require 'travis/scheduler/serialize/worker/repo'
         require 'travis/scheduler/serialize/worker/ssh_key'
-        require 'travis/remote_vcs/repository'
 
         def data
           data = {
@@ -133,7 +132,7 @@ module Travis
           end
 
           def source_host
-            vcs_source_host['host_name'] || config[:github][:source_host] || 'github.com'
+            repo.vcs_source_host || config[:github][:source_host] || 'github.com'
           end
 
           def cache_settings
@@ -169,12 +168,6 @@ module Travis
 
           def compact(hash)
             hash.reject { |_, value| value.nil? }
-          end
-
-          def vcs_source_host
-            @vcs_source_host ||= Travis::RemoteVCS::Repository.new(config).meta(repo.id)
-          rescue
-            {}
           end
       end
     end
