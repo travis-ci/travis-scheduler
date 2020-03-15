b5e04b4fe15589496629fd67107fc0b1ca6e68a0
Artur Kremens <artur@assembla.com>
2020-03-15 17:30:44 +0100

    fix

--- lib/travis/scheduler/record/pull_request.rb
+++ lib/travis/scheduler/record/pull_request.rb
@@ -14,7 +14,7 @@ class PullRequest < ActiveRecord::Base
     puts "FIND REPO WITH SLUG #{head_repo_slug}"
     head_repository = Repository.find_by(vcs_slug: head_repo_slug)
     puts "DO WE HAVE IT?? #{head_repository}"
-    puts "IS IT PRIVATE #{(head_repository&.private?}"
+    puts "IS IT PRIVATE #{head_repository&.private?}"
     puts "AND WE SHOULD RETURN GIT? #{repo.git_url?(head_repository&.private?)}"
     repo.git_url?(head_repository&.private?) ? head_git_url(repo) : head_http_url(repo)
   end
