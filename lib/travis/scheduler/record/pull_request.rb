f0caa1af8d167c67f43981953ddc541f05c82dff
Artur Kremens <artur@assembla.com>
2020-03-16 12:37:04 +0100

    Use only git url

--- lib/travis/scheduler/record/pull_request.rb
+++ lib/travis/scheduler/record/pull_request.rb
@@ -2,17 +2,7 @@ class PullRequest < ActiveRecord::Base
   belongs_to :repository
   has_many :requests
 
-  def head_git_url(repo)
-    repo.source_git_url(head_repo_slug)
-  end
-
-  def head_http_url(repo)
-    repo.source_http_url(head_repo_slug)
-  end
-
   def head_url(repo)
-    # head_repository = Repository.find_by(vcs_slug: head_repo_slug)
-    # head_repository&.private? ? head_git_url(repo) : head_http_url(repo)
-    head_git_url(repo)
+    repo.source_git_url(head_repo_slug)
   end
 end
