513855174cf4ea6f29f11f6e4d58c7417c2e8f82
Tahsin Hasan <51903216+Tahsin-travis-ci@users.noreply.github.com>
2020-05-15 22:04:45 +0600

    source url for github enterprise (#232)
    * source url for github enterprise
    
    * installation id for ghe
    
    * installation id for ghe
    
    * installation id for ghe

--- lib/travis/scheduler/serialize/worker/repo.rb
+++ lib/travis/scheduler/serialize/worker/repo.rb
@@ -26,6 +26,7 @@ module Travis
           end
 
           def source_url
+            return source_git_url if force_private? && !Travis.config.prefer_https
             return source_http_url if Travis.config.prefer_https || managed_by_app?
             (repo.private? || force_private?) ? source_git_url : source_http_url
           end
@@ -39,7 +40,7 @@ module Travis
           end
 
           def installation_id
-            repo.installation&.github_id if repo.managed_by_app? && repo.private
+            repo.installation&.github_id if repo.managed_by_app? && (repo.private || force_private?)
           end
 
           def github?
