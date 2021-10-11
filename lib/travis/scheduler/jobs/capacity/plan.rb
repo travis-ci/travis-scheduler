dea4722998f18d0f88e08101c59e3e09ff2c72e2
AndriiMysko <usandrewmisko@gmail.com>
2021-10-11 13:24:08 +0300

    Fix stuck jobs due to billing

--- lib/travis/scheduler/jobs/capacity/plan.rb
+++ lib/travis/scheduler/jobs/capacity/plan.rb
@@ -12,13 +12,27 @@ module Travis
           end
 
           def accept?(job)
-            super if !on_metered_plan? || billing_allowance[allowance_key(job)]
+            super if !on_metered_plan? || billing_allowed?(job)
           end
 
           private
 
             def max
               @max ||= on_metered_plan? ? billing_allowance['concurrency_limit'] : owners.paid_capacity
+            end
+
+            def billing_allowed?(job)
+              puts billing_allowance[allowance_key(job)]
+              return true if billing_allowance[allowance_key(job)]
+
+              # Cancel job if it has not been queued for more than a day due to
+              # billing allowance
+              if job.created_at < (Time.now - 1.day)
+                payload = { id: job.id, source: 'scheduler' }
+                Hub.push('job:cancel', payload)
+              end
+
+              false
             end
 
             def allowance_key(job)
