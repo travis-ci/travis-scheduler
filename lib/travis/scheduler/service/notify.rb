b88b6d073fd8d8ee0eade9748bf582179d5a12e6
gabriel-arc <gabriel@travis-ci.org>
2021-11-15 13:19:03 +0100

    dbg

--- lib/travis/scheduler/service/notify.rb
+++ lib/travis/scheduler/service/notify.rb
@@ -43,7 +43,11 @@ module Travis
 
           def notify_rabbitmq
             info :publish, job.id, job.queue, 'rabbitmq'
-            amqp.publish(worker_payload, properties: { type: 'test', persistent: true })
+            puts "PUBLISH!!!"
+            ply = worker_payload
+            puts "PLY1"
+            puts "PLY: #{ply.inspect}"
+            amqp.publish(ply, properties: { type: 'test', persistent: true })
           end
 
           def notify_live
