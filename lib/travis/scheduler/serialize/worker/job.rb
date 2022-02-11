b647ac6db9049c13d96c37a5ad0c108f12dc2b9f
gabriel-arc <gabriel@travis-ci.org>
2022-02-11 15:13:41 +0100

    dbg

--- lib/travis/scheduler/serialize/worker/job.rb
+++ lib/travis/scheduler/serialize/worker/job.rb
@@ -49,6 +49,7 @@ module Travis
           end
 
           def decrypt(str)
+            puts "DECRYPTING USING KEY: #{repository_key&.data.inspect}\n\n"
             repository_key.decrypt(Base64.decode64(str)) if str.is_a?(String)
           rescue OpenSSL::PKey::RSAError => e
           end
