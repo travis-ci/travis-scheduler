1b5953e2f35b9740d154d5984888f160ac366944
gabriel-arc <gabriel@travis-ci.org>
2022-02-18 15:07:59 +0100

    share fix

--- lib/travis/scheduler/helper/job_repository.rb
+++ lib/travis/scheduler/helper/job_repository.rb
@@ -9,17 +9,20 @@ module Travis
 
           return unless job.source.request.pull_request&.repository_id
 
-          pr_repository = ::Repository.find_by(github_id: job.source.request.pull_request.head_repo_github_id);
 
-          return unless pr_repository
 
           base_repo = ::Repository.find(job.source.request.pull_request.repository_id)
 
           return unless base_repo
 
+          return base_repo if base_repo.settings.share_ssh_keys_with_forks
+
+          pr_repository = ::Repository.find_by(github_id: job.source.request.pull_request.head_repo_github_id);
+
+          return unless pr_repository
+
           return pr_repository if job.source.request.pull_request.head_repo_github_id == base_repo.github_id
 
-          return base_repo if base_repo.settings.share_ssh_keys_with_forks
 
           owner_name, repo_name = job.source.request.pull_request.head_repo_slug.split('/')
           return pr_repository if owner_name.nil? || owner_name.empty? || repo_name.nil? || repo_name.empty?
