61783a558dbf3e855943d69957c89656d4aede91
gabriel-arc <57348209+GbArc@users.noreply.github.com>
2022-03-10 12:56:03 +0100

    sharing envs to forks (#272)
    * repo settings merge from master

--- lib/travis/scheduler/serialize/worker.rb
+++ lib/travis/scheduler/serialize/worker.rb
@@ -22,7 +22,7 @@ module Travis
             host: Travis::Scheduler.config.host,
             source: build_data,
             repository: repository_data,
-            ssh_key: ssh_key.data,
+            ssh_key: ssh_key&.data || nil,
             timeouts: repo.timeouts,
             cache_settings: cache_settings,
             workspace: workspace,
@@ -108,7 +108,7 @@ module Travis
 
           def source_url
             # TODO move these things to Build
-            return repo.source_git_url if repo.private? && ssh_key.custom?
+            return repo.source_git_url if repo.private? && ssh_key&.custom?
             repo.source_url
           end
 
@@ -133,7 +133,25 @@ module Travis
           end
 
           def ssh_key
-            @ssh_key ||= SshKey.new(repo, job, config)
+            selected_repo = ssh_key_repository
+            return nil unless selected_repo
+
+            @ssh_key ||= SshKey.new(Repo.new(selected_repo, config), job, config)
+          end
+
+          def ssh_key_repository
+            return job.repository if job.source.event_type != 'pull_request' || job.source.request.pull_request.head_repo_slug == job.source.request.pull_request.base_repo_slug
+
+            base_repo_owner_name, base_repo_name = job.source.request.pull_request.base_repo_slug.to_s.split('/')
+            return job.repository if base_repo_owner_name.nil? || base_repo_owner_name.empty? || base_repo_name.nil? || base_repo_name.empty?
+            base_repo = ::Repository.find_by(owner_name: base_repo_owner_name, name: base_repo_name)
+            return job.repository if base_repo.nil?
+            return base_repo if base_repo.settings.share_ssh_keys_with_forks
+
+            head_repo_owner_name, head_repo_name = job.source.request.pull_request.head_repo_slug.to_s.split('/')
+            return job.repository if head_repo_owner_name.nil? || head_repo_owner_name.empty? || head_repo_name.nil? || head_repo_name.empty?
+
+            ::Repository.find_by(owner_name: head_repo_owner_name, name: head_repo_name) || nil
           end
 
           def source_host
@@ -179,7 +197,7 @@ module Travis
             @allowed_repositories ||= begin
               repository_ids = Repository.where(owner_id: build.owner_id, active: true).select{ |repo| repo.settings.allow_config_imports }.map(&:vcs_id)
               repository_ids << repo.vcs_id
-              repository_ids.uniq.sort              
+              repository_ids.uniq.sort
             end
           end
       end
