a8c777ec9ad3dc86077e6a4c7f1293a1a2447d56
gabriel-arc <57348209+GbArc@users.noreply.github.com>
2025-01-20 11:30:18 +0100

    redis tls (#322)
    Co-authored-by: GbArc <gabriel.arczynski@devtactics.net>
    Co-authored-by: piccadilly-circus <134370605+piccadilly-circus@users.noreply.github.com>

--- lib/travis/scheduler/helper/locking.rb
+++ lib/travis/scheduler/helper/locking.rb
@@ -9,8 +9,15 @@ module Travis
         def exclusive(key, config, opts = {}, &block)
           options = config[:lock].to_h
           options = options.merge(opts)
-          options[:url] ||= config[:redis][:url] if options[:strategy] == :redis
           options[:ttl] ||= 2
+          if options[:strategy] == :redis
+            options[:url] ||= config[:redis][:url]
+            options[:ssl] ||= config[:redis][:ssl]
+            options[:ca_path] ||= ENV['REDIS_SSL_CA_PATH'] if ENV['REDIS_SSL_CA_PATH']
+            options[:cert] ||= OpenSSL::X509::Certificate.new(File.read(ENV['REDIS_SSL_CERT_FILE'])) if ENV['REDIS_SSL_CERT_FILE']
+            options[:key] ||= OpenSSL::PKEY::RSA.new(File.read(ENV['REDIS_SSL_KEY_FILE'])) if ENV['REDIS_SSL_KEY_FILE']
+            options[:verify_mode] ||= OpenSSL::SSL::VERIFY_NONE if config[:ssl_verify] == false
+          end
           info "Locking #{key} with: #{options[:strategy]}, ttl: #{options[:ttl]}s"
           options[:ttl] = options[:ttl] * 1000 # RedLock wants milliseconds
           Lock.exclusive(key, options, &block)
