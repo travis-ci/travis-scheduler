578d198d74ad4ac32b66cc263326e472464089e2
nikolaTrichkovski <nikola.trichkovski@devtactics.net>
2025-03-26 20:01:24 +0100

    use job configs for keep_netrc

--- lib/travis/scheduler/serialize/worker.rb
+++ lib/travis/scheduler/serialize/worker.rb
@@ -32,7 +32,7 @@ module Travis
             workspace:,
             enterprise: !!config[:enterprise],
             prefer_https: !!config[:prefer_https],
-            keep_netrc: repo.keep_netrc?,
+            keep_netrc: job.decrypted_config.key?(:keep_netrc) ? job.decrypted_config[:keep_netrc] : repo.keep_netrc?,
             secrets: job.secrets,
             allowed_repositories:
           }
